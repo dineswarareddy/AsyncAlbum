15f5a630308047832a057dfc91c08f0e8f6cc3c1
Dinesh Reddy Chennuru <dineswarareddy@gmail.com>
2019-08-03 15:27:03 +0530

    :gift: Application initial setup done.
    Created application and pushed same to the repository.

---
+++ AsyncAlbum/AsyncAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AsyncAlbum
+//
+//  Created by Dinesh Reddy on 03/08/19.
+//  Copyright © 2019 Dinesh Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
