1a3d90c1d732fc82ced8d4e6689d4a89adf751df
Dinesh Reddy Chennuru <dineswarareddy@gmail.com>
2019-08-03 23:23:07 +0530

    :gift: Configuration file.
    Helps to configure application properties.

---
+++ AsyncAlbum/UsersKit/UserListConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  UserListConfiguration.swift
+//  AsyncAlbum
+//
+//  Created by Dinesh Reddy on 03/08/19.
+//  Copyright © 2019 Dinesh Reddy. All rights reserved.
+//
+
+import Foundation
+
+final class UserListConfiguration {
+    static let userListURL = "http://pastebin.com/raw/wgkJgazE"
+    static let maxStorageCapacity = 1024 * 40
+    static let userListPaginationCount = 10
+}
