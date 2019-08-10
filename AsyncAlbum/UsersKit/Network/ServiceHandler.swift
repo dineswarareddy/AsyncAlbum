772b0277dcce9d910530e152b678e946fa55ef83
Dinesh Reddy Chennuru <dineswarareddy@gmail.com>
2019-08-10 21:33:56 +0530

    :gift:  Added JSON response cache.

--- AsyncAlbum/UsersKit/Network/ServiceHandler.swift
+++ AsyncAlbum/UsersKit/Network/ServiceHandler.swift
@@ -15,15 +15,22 @@ enum DefinedError: String, Error {
 }
 
 class ServiceHandler: NSObject {
+    private override init() { }
+    static let shared = ServiceHandler()
     typealias ServiceCallBack = (_ response: Any?, _ error: Error?) -> (Void)
-    
-    class func fetchUsersList(requestURL: URL, parameters: [String: Any] = [:], completion: @escaping ServiceCallBack) {
+    static var cacheResponse: [String: Any]? = [:]
+    static func fetchUsersList(requestURL: URL, useCache: Bool = false, parameters: [String: Any] = [:], completion: @escaping ServiceCallBack) {
+        if useCache, let existingResponse = cacheResponse?[requestURL.absoluteString] {
+                completion(existingResponse, nil)
+                return
+        }
         Alamofire.request(requestURL,
                           method: .get,
                           parameters: parameters)
             .validate()
             .responseJSON { response in
                 guard response.result.isFailure else {
+                    cacheResponse?[requestURL.absoluteString] = response.result.value
                     completion(response.result.value, nil)
                     return
                 }
